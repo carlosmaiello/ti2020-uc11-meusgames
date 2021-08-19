6c04c380f5d14c34c1637fa45c89091e8537d9b5
CARLOS ALBERTO MAIELLO JUNIOR <carlos.amjunior2@senacsp.edu.br>
2021-08-18 21:58:14 -0300

    'Perfil'

--- src/pages/Perfil.vue
+++ src/pages/Perfil.vue
@@ -1,11 +1,19 @@
 <template>
   <q-page padding>
     <h2 class="q-mt-md">Perfil</h2>
+    <h4>{{ usuario.username }}</h4>
+    <p><b>Nome</b>: {{ usuario.first_name }} {{ usuario.last_name }}</p>
+    <p><b>E-mail</b>: {{ usuario.email }}</p>
   </q-page>
 </template>
 
 <script>
 export default {
   // name: 'PageName',
+  computed: {
+    usuario () {
+      return this.$store.state.usuarios.usuario;
+    }    
+  }
 };
 </script>
--- src/store/usuarios/actions.js
+++ src/store/usuarios/actions.js
@@ -6,10 +6,16 @@ export function someAction (context) {
 import { api } from "boot/axios";
 const storage = window.localStorage;
 
-export function logar({ commit }, { username, password }) {
+export function logar({ commit, dispatch }, { username, password }) {
   return api.post("/login/", { username, password }).then(r => {
     commit("setToken", r.data.token);
     storage.setItem("token", r.data.token);
+
+    return dispatch("me").catch(error => {
+      console.log("error");
+      commit("setToken", null);
+      return error;
+    });
   });
 }
 
